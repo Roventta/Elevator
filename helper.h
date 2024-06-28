8b66d9b58c0c83e755a21c14475f6becd14768b4
Roventta <u6740827@anu.edu.au>
2024-06-28 23:01:42 +1000

    tested MPU lib, added a helper file, PlotterWrapper is a class for easier serial monitoring

---
+++ helper.h
@@ -0,0 +1,49 @@
+#ifndef SPW_h
+#define SPW_h
+
+#define BUFSIZE 10
+
+#include <Arduino.h>
+
+template <typename T>
+class SerialPlotterWrapper{
+
+  private: 
+  char** Fields;
+  int MaxSize;
+  int CurSize = 0;
+  T** FieldReadings;
+
+  public:
+  SerialPlotterWrapper(int size){
+    this->MaxSize = size;
+    this -> Fields = (char**) malloc(size * sizeof(char*));
+    for(int i=0; i<size; i++){
+      this-> Fields[i] = (char*) malloc(BUFSIZE* sizeof(char));
+    }
+
+    this -> FieldReadings = (T**) malloc(size * sizeof(T*));
+  }
+
+  void addField(char *fieldName, T *fieldRef){
+    if(this->CurSize < this->MaxSize){
+      strcpy(Fields[CurSize], fieldName);
+      FieldReadings[CurSize] = fieldRef;
+      CurSize++;
+    }
+  }
+
+  void emitPlot(){
+    for(int i=0; i<CurSize; i++){
+      Serial.print(Fields[i]);
+      Serial.print(":");
+      Serial.print(*FieldReadings[i]);
+
+      if(i != CurSize-1){Serial.print(",");};
+    }
+    Serial.println();
+  }
+
+};
+
+#endif
